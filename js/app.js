5956727082cc144c34bf820cbc751f5a286297dc
Naldson Chagas <naldson@protonmail.com>
2018-12-28 18:05:00 -0300

    show lod for user when is load all states

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
 import { PersonController } from "./controllers/PersonController";
 
 const controller = new PersonController();
-
-controller.getStates();
--- js/controllers/PersonController.js
+++ js/controllers/PersonController.js
@@ -2,6 +2,10 @@ import { AddressService } from "./../service/AddressService";
 
 export class PersonController {
 
+  constructor() {
+    this.getStates();
+  }
+
   addStatesInSelect(states) {
     const stateEl = document.querySelector('#state');
 
@@ -11,27 +15,35 @@ export class PersonController {
       option.value = UF.id;
       option.text = UF.sigla;
 
-      stateEl.add(option);
-    
+      stateEl.add(option);    
     });
   }
 
   getStates() {
     const states = []
-
+    this.showLoad(true);
     AddressService.getStates()
     .then((result) => {
+
       JSON.parse(result).forEach(state => {
         states.push(state);
       });
-
       this.addStatesInSelect(states.sort((a, b) => {
         if (a.sigla < b.sigla) return -1;
         return 1;
       }));
+
+      this.showLoad(false);
     }).catch((err) => {
       console.log(err);
-      
     });
+  }
+
+  showLoad(show) {
+    const loadEl = document.querySelector('.load');
+    if (show) loadEl.classList.remove('invisible');
+    else 
+      if (!loadEl.classList.contains('invisible')) 
+        loadEl.classList.add('invisible');
   }
 }
