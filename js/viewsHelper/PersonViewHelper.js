39c511712e5b0acd6df0cf8ad826c22432efc319
Naldson Chagas <naldson@protonmail.com>
2018-12-28 20:31:38 -0300

    cpf mask

--- js/viewsHelper/PersonViewHelper.js
+++ js/viewsHelper/PersonViewHelper.js
@@ -3,6 +3,12 @@ export class PersonViewHelper {
   constructor() {
     this._citySelect = document.querySelector('#city');
     this._stateSelect = document.querySelector('#state');
+
+    this._telephone = document.querySelector('#telephone');
+    this._cpf = document.querySelector('#cpf');
+
+    this.cpfMask();
+    this.telephoneMask();
   }
 
   addInSelect(options) {
@@ -22,6 +28,41 @@ export class PersonViewHelper {
         this._citySelect.add(optionEl);
       }
     });
+  }
+
+  cpfMask() {
+    this._cpf.addEventListener('input', () => {
+      if (isNaN(this._cpf.value.slice(-1))) {
+        this._cpf.value = this._cpf.value
+          .substring(0, this._cpf.value.length - 1);
+        return;
+      }
+
+      if (this._cpf.value.length == 3 || this._cpf.value.length == 7) {
+        this._cpf.value += '.';
+      } else if (this._cpf.value.length == 11) {
+        this._cpf.value += '-';
+      }
+
+      if (this._cpf.value.charAt(3) != '.' && this._cpf.value.length > 3) {
+        this._cpf.value = this._cpf.value.slice(0, 3) + '.' + 
+        this._cpf.value.slice(4);
+      }
+
+      if (this._cpf.value.charAt(7) != '.' && this._cpf.value.length > 7) {
+        this._cpf.value = this._cpf.value.slice(0, 7) + '.' + 
+        this._cpf.value.slice(8);
+      }
+
+      if (this._cpf.value.charAt(11) != '-' && this._cpf.value.length > 11) {
+        this._cpf.value = this._cpf.value.slice(0, 11) + '.' + 
+        this._cpf.value.slice(12);
+      }
+    });
+  }
+
+  telephoneMask() {
+
   }
 
   static showLoad(show) {
